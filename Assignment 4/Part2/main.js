045406eb5cf8493619422d3c30b487ec114067ce
NirmaljotSingh <nirmaljotsingh@dcmail.ca>
2024-03-30 19:18:46 -0400

    I declared arrays.
    I declared arrays for listing image files and array for alternative text for image files.

--- Assignment 4/Part2/main.js
+++ Assignment 4/Part2/main.js
@@ -5,8 +5,13 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageArray = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+
 
 /* Declaring the alternative text for each image file */
+const alternativeTextArray = ['Closeshot of a human eye', 'A beautifull landscape', 'Purple and White Flowers', 'Egyptians', 'Butterfly'];
+
+
 
 /* Looping through images */
 
